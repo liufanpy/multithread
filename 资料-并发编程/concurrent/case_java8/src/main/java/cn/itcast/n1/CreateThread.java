2d78766c8d1e2feaeb12b8ddacf871b8f45d3985
“liufan” <70306977+liufanpy@users.noreply.github.com>
2021-10-28 22:36:47 +0800

    JUC并发编程资料

---
+++ 资料-并发编程/concurrent/case_java8/src/main/java/cn/itcast/n1/CreateThread.java
@@ -0,0 +1,14 @@
+package cn.itcast.n1;
+
+public class CreateThread {
+
+    public static void main(String[] args) {
+        Thread thread = new Thread(){
+
+        };
+        thread.start();
+
+
+    }
+
+}
